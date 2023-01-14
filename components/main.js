d6b0375d610c05e618dc9742152232aa344d4043
Codehash001 <codehashblockchain@gmail.com>
2023-01-14 08:31:46 +0530

    update

--- components/main.js
+++ components/main.js
@@ -39,7 +39,7 @@ export default function Home(){
             </div>
             <div className="lg:mx-8 md:mx-4 mx-2 md:mt-0 mt-2">
                 <img src="/bg.gif"
-                className='md:w-full h-full w-[400px] h-[500px]'/>
+                className='md:w-[600px] h-full w-[400px] h-[500px]'/>
             </div>
         </div>
         </div>
