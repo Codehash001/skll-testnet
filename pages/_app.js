11f38dc56f03eb3ff1f315f992fa9009a08c408c
Codehash001 <codehashblockchain@gmail.com>
2023-01-14 08:58:12 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes'
 export default function App({ Component, pageProps }) {
   return (
    
-    <div className='w-srceen'>
+    <div className='w-srceen overflow-x-hidden'>
   <ThemeProvider enableSystem={true} attribute='class'>
   <Component {...pageProps} />
   </ThemeProvider>
