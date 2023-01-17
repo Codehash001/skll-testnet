3cb94074750f2ca4d8855888d424ffad14f86885
Codehash001 <codehashblockchain@gmail.com>
2023-01-17 13:57:42 +0530

    update

--- components/main.js
+++ components/main.js
@@ -41,8 +41,8 @@ export default function Home(){
                 </div>
             </div>
             <div className="lg:mx-8 md:mx-4 mx-2 md:mt-0 mt-2">
-                <img src="/bg.gif"
-                className='md:w-[1000px] md:h-[400px] w-[400px] h-[500px]'/>
+                <img src="/bg-gif.gif"
+                className='w-[500px] h-[500px]'/>
             </div>
         </div>
         </div>
--- components/slide.js
+++ components/slide.js
@@ -47,15 +47,9 @@ export default function SimpleSlider() {
   <div className="md:mx-4 grid grid-cols-1 lg:grid-cols-5 gap-5 md:mt-0 mt-2">
     
    {data.map((item) =>
-   <div key={item.id}className="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500" data-aos="fade-up">
+   <div key={item.id}className="max-w-sm bg-transpaent px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500" data-aos="fade-up">
       <div className="relative">
-        <img className="md:w-[200px] md:h-[200px] w-auto h-auto rounded-xl" src={item.image}alt="img" />
-        <p className="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">1 Eth</p>
-      </div>
-      <h1 className="mt-4 text-gray-800 text-sm text-center">{item.description}</h1>
-      <div className="my-4">
-      <Link className="cursor-pointer" activeClass ="active" to='mint' spy={true} smooth={true} offset={-70} duration={500}>
-       <button className="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button> </Link>
+        <img className="md:w-[250px] md:h-[250px] w-auto h-auto rounded-xl" src={item.image}alt="img" />
       </div>
     </div>
     )}
