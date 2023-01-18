e9c95f3641f83816ea19ef59872c5e2ca3491e22
Codehash001 <codehashblockchain@gmail.com>
2023-01-18 20:30:03 +0530

    css fix

--- pages/privacy.js
+++ pages/privacy.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="Description" content="Skulls Klubs LimitLess!" />
         <link rel="icon" href="/Logoicon.png" />
       </Head>
-      <div className= 'flex flex-col items-center md:mx-16 mx-2 font-Archivo [word-spacing:2px]'>
+      <div className= 'flex flex-col items-center md:mx-16 mx-2 font-Archivo [word-spacing:2px] my-[40px]'>
 
         <h1 className='text-center text-3xl font-bold'>INSIDER TRADING POLICY</h1>
         <h1 className='text-center text-2xl font-bold'>LAST UPDATED: JAN 16, 2023</h1>
@@ -174,10 +174,10 @@ In the event that Company determines that an Insider violated the NFT Insider Tr
       </div>
       <Footer/>
     
-      <div class="dark:hidden blob w-[800px] h-[800px] rounded-[999px] absolute top-0 right-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200"></div>
-    <div class="dark:hidden blob w-[1000px] h-[1000px] rounded-[999px] absolute bottom-0 left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-red-200 via-gray-100 to-blue-100"></div>
-    <div class="dark:hidden blob w-[600px] h-[600px] rounded-[999px] absolute bottom-0 left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-slate-100 via-teal-100 to-blue-100"></div>
-    <div class="dark:hidden blob w-[300px] h-[300px] rounded-[999px] absolute bottom-[-10px] left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-green-200 via-cyan-200 to-Fuchsia-300"></div>
+      <div class="dark:hidden hidden md:flex blob w-[800px] h-[800px] rounded-[999px] absolute top-0 right-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200"></div>
+    <div class="dark:hidden hidden md:flex blob w-[1000px] h-[1000px] rounded-[999px] absolute bottom-0 left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-red-200 via-gray-100 to-blue-100"></div>
+    <div class="dark:hidden hidden md:flex blob w-[600px] h-[600px] rounded-[999px] absolute bottom-0 left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-slate-100 via-teal-100 to-blue-100"></div>
+    <div class="dark:hidden hidden md:flex blob w-[300px] h-[300px] rounded-[999px] absolute bottom-[-10px] left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-green-200 via-cyan-200 to-Fuchsia-300"></div>
     </div>
   )
 }
